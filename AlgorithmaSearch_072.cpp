64a4ff2f56ee79705c5a635c5cbb1d3e66584dce
Yurcell <chestayurcel@gmail.com>
2024-04-05 15:12:23 +0700

    membuat step 6 dari perintah do while

--- AlgorithmaSearch_072.cpp
+++ AlgorithmaSearch_072.cpp
@@ -46,6 +46,9 @@ void LinearSearch()
         {
             ctr++;
             if (arr[i] == item)
+            {
+                cout << "\n" << item << " found at position " << (i + 1) << endl; // step 6
+            }
         }
     }while ()
 
